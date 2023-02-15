4d0a392507f479d97ff8f7bae39632864155ff1f
leonTech254 <martinleontech23@gmail.com>
2023-02-15 15:52:37 +0300

    port scanner using python-nmap

---
+++ banner_nmap.py
@@ -0,0 +1,22 @@
+import nmap
+scanner=nmap.PortScanner()
+scanner.scan("197.136.17.40",arguments='-sV')
+
+def leonSeperater(result):
+    s="="
+    for i in range(0,100):
+        s+="="
+    print(result)   
+    print(s)
+for host in scanner.all_hosts():
+    print(host)
+    for port in scanner[host]['tcp']:
+        allData=scanner[host]['tcp'][port]
+        result=f"""
+Port number {port}\n {allData}
+        """
+        leonSeperater(result=result)
+    
+       
+        
+            
